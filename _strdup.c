c9eeb24d2849424bdb8824208e500b49863aa0ae
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-05-24 16:54:10 +0300

    sff

---
+++ _strdup.c
@@ -0,0 +1,24 @@
+#include "shell.h"
+
+/**
+ * _strdup - duplicates a string
+ * @str: the string to duplicate
+ *
+ * Return: pointer to the duplicated string
+ */
+char *_strdup(const char *str)
+{
+	int length = 0;
+	char *ret;
+
+	if (str == NULL)
+		return (NULL);
+	while (*str++)
+		length++;
+	ret = malloc(sizeof(char) * (length + 1));
+	if (!ret)
+		return (NULL);
+	for (length++; length--;)
+		ret[length] = *--str;
+	return (ret);
+}
