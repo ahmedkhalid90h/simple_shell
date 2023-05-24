4da182ef05b63a59fa3561fae58507a17d915663
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-05-24 12:20:09 +0300

    df

--- free_str_list.c
+++ free_str_list.c
@@ -6,12 +6,14 @@
  */
 void free_str_list(char **str_ing)
 {
-	char **a = str_ing;
-	int i;
-	if (!str_ing)
-		return;
+char **a = str_ing;
+int i;
+if (!str_ing)
+{
+return;
+}
     for (i = 0; str_ing[i]; i++) {
-		free(str_ing[i]);
-	}
-	free(a);
+free(str_ing[i]);
+}
+free(a);
 }
