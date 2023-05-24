8146b6957ca291b5f408b719abdcd8370f0330d1
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-05-24 11:54:31 +0300

    sdf

--- find_commd.c
+++ find_commd.c
@@ -8,40 +8,46 @@
  */
 void find_commd(info_t *info)
 {
-    char *path = NULL;
-    int counter = 0, k = 0;
+char *path = NULL;
+int counter = 0, k = 0;
 
-    info->path_f = info->argv[0];
-    if (info->linecount_flag == 1)
-    {
-        info->line_count_++;
-        info->linecount_flag = 0;
-    }
+info->path_f = info->argv[0];
+if (info->linecount_flag == 1)
+{
+info->line_count_++;
+info->linecount_flag = 0;
+}
 
-    while (info->arg[counter])
-    {
-        if (!is_delimeter_char(info->arg[counter], " \t\n"))
-            k++;
-        counter++;
-    }
-    if (!k)
-        return;
+while (info->arg[counter])
+{
+if (!is_delimeter_char(info->arg[counter], " \t\n"))
+{
+k++;
+}
+counter++;
+}
+if (!k)
+{
+return;
+}
 
-    path = find_path_f(info, _getenv(info, "PATH="), info->argv[0]); 
-    if (path)
-    {
-        info->path_f = path;
-        create_child_fork(info);
-    }
-    else
-    {
-        if ((interactive_mode(info) || _getenv(info, "PATH=") /* up to __getenv */
-            || info->argv[0][0] == '/') && is_cmd_(info, info->argv[0]))
-            create_child_fork(info);
-        else if (*(info->arg) != '\n')
-        {
-            info->status = 127;
-            print_err(info, "not found\n");
-        }
-    }
+path = find_path_f(info, _getenv(info, "PATH="), info->argv[0]); 
+if (path)
+{
+info->path_f = path;
+create_child_fork(info);
+}
+else
+{
+if ((interactive_mode(info) || _getenv(info, "PATH=") /* up to __getenv */
+|| info->argv[0][0] == '/') && is_cmd_(info, info->argv[0]))
+{
+create_child_fork(info);
+}
+else if (*(info->arg) != '\n')
+{
+info->status = 127;
+print_err(info, "not found\n");
+}
+}
 }
