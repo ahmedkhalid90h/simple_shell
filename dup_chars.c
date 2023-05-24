863f5f4b0770801752f894b28d713856308de984
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-05-24 11:02:20 +0300

    dfgd

--- dup_chars.c
+++ dup_chars.c
@@ -10,15 +10,18 @@
  */
 char *dupl_chars(char *path_str, int start_idx, int stop_idx)
 {
-    static char buf[1024];
-    int k = 0;
-    int i = start_idx;
+static char buf[1024];
+int k = 0;
+int i = start_idx;
 
-    while (i < stop_idx) {
-        if (path_str[i] != ':')
-            buf[k++] = path_str[i];
-        i++;
-    }
-    buf[k] = '\0';
-    return buf;
+while (i < stop_idx)
+{
+if (path_str[i] != ':')
+{
+buf[k++] = path_str[i];
+}
+i++;
+}
+buf[k] = '\0';
+return (buf);
 }
