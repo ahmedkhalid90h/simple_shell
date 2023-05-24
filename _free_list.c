3589ac08c8c13fbd2d5a4547ab171d063da8ae41
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-05-24 12:16:33 +0300

    ds

--- _free_list.c
+++ _free_list.c
@@ -8,16 +8,18 @@
  */
 void free_list_node(list_t **head_ptr)
 {
-	list_t *current_node, *next_node, *head;
+list_t *current_node, *next_node, *head;
 
-	if (!head_ptr || !*head_ptr)
-		return;
-	head = *head_ptr;
-	for (current_node = head; current_node; current_node = next_node)
-	{
-		next_node = current_node->next;
-		free(current_node->str);
-		free(current_node);
-	}
-	*head_ptr = NULL;
+if (!head_ptr || !*head_ptr)
+{
+return;
+}
+head = *head_ptr;
+for (current_node = head; current_node; current_node = next_node)
+{
+next_node = current_node->next;
+free(current_node->str);
+free(current_node);
+}
+*head_ptr = NULL;
 }
