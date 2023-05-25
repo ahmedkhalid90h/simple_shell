0123e706ccaa71aaeba044b74fec138c9f8df6f4
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-05-25 03:18:09 +0300

    sdad

--- _getline.c
+++ _getline.c
@@ -14,59 +14,28 @@ static size_t i, len;
 size_t k;
 ssize_t r = 0, s = 0;
 char *p = NULL, *new_p = NULL, *c;
-
 p = *ptq;
 if (p && length)
-{
 s = *length;
-}
 if (i == len)
-{
 i = len = 0;
-}
 r = read_buf(info, buf, &len);
 if (r == -1 || (r == 0 && len == 0))
-{
 return (-1);
-}
 c = _strchr(buf + i, '\n');
 k = c ? 1 + (unsigned int)(c - buf) : len;
 new_p = _realloc(p, s, s ? s + k : k + 1);
 if (!new_p) /* MALLOC FAILURE! */
-{
 return (p ? free(p), -1 : -1);
-}
-
 if (s)
-{
 _strncat(new_p, buf + i, k - i);
-}
 else
-{
 _strncpy(new_p, buf + i, k - i + 1);
-}
 s += k - i;
 i = k;
 p = new_p;
 if (length)
-{
 *length = s;
-}
 *ptq = p;
 return (s);
-}
-/**
- **_strchr - locates a character in a string
- *@sq: the string to be parsed
- *@cdc: the character to look for
- *Return: (s) a pointer to the memory area s
- */
-char *_strchr(char *sq, char cdc)
-{
-do {
-if (*sq == cdc)
-return (sq);
-} while (*sq++ != '\0');
-
-return (NULL);
 }
