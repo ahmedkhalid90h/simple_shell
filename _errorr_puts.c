3799f3dfe7c764d966004d0a873ac278365e40db
Mohamed Mansour <moyamed2mansour@gmail.com>
2023-05-25 19:48:21 +0300

    ff

--- _errorr_puts.c
+++ _errorr_puts.c
@@ -8,16 +8,16 @@
  */
 void _puts_error(char *str)
 {
-	int i = 0;
+int i = 0;
 
-	if (!str)
-	{
-    return;
-    }
-	while (str[i] != '\0')
-	{
-		putchar_error(str[i]);
-		i++;
-	}
+if (!str)
+{
+return;
+}
+while (str[i] != '\0')
+{
+putchar_error(str[i]);
+i++;
+}
 }
 
