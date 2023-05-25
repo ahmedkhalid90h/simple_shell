a83b1679d7f19ed2e5f601cedb8fe287f381bb21
ahmedkhalid90h <ahmedkhalid90h@gmail.com>
2023-05-25 23:27:36 +0300

    goooooooooo

--- shell.c
+++ shell.c
@@ -13,6 +13,27 @@ int main(int argc, char **argv)
 	NULL, NULL, NULL, NULL, 0, 0, NULL, 0, 0}};
 	int s_fd;
 
+	if (argc == 2)
+	{
+		s_fd = open(argv[1], O_RDONLY);
+		if (s_fd == -1)
+		{
+			if (errno == EACCES)
+			{
+				exit(126);
+			}
+			if (errno == ENOENT)
+			{
+				_puts_error(argv[0]);
+				_puts_error(": 0: Can't open ");
+				_puts_error(argv[1]);
+				putchar_error('\n');
+				putchar_error(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
+		}
+	}
 	populate_env(info);
 	hsh(info, argv);
 	return (EXIT_SUCCESS);
